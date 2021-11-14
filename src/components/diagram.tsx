fb4170a08dbd4b2871c0b7102267ce11066c7c50
Scott Cytacki <scytacki@concord.org>
2021-11-13 22:38:30 -0500

    basic custom node from examples

--- src/components/diagram.tsx
+++ src/components/diagram.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactFlow from "react-flow-renderer";
+import { QuantityNode } from "./quantity-node";
 
 const elements = [
   {
@@ -21,13 +22,26 @@ const elements = [
     data: { label: "Output Node" },
     position: { x: 250, y: 250 },
   },
+  {
+    id: "4",
+    type: "quantityNode",
+    position: { x: 100, y: 250 },
+    data: { color: "#1A192B" }
+  },
   // animated edge
   { id: "e1-2", source: "1", target: "2", animated: true },
   { id: "e2-3", source: "2", target: "3" },
+  { id: "e2-4", source: "2", target: "4" },
+  { id: "e4-3", source: "4", target: "3", sourceHandle: "b" }
 ];
+
+const nodeTypes = {
+  quantityNode: QuantityNode,
+};
 
 export const Diagram = () => (
   <div style={{ height: 300, width: 600 }}>
-    <ReactFlow elements={elements} />
+    <ReactFlow elements={elements} 
+      nodeTypes={nodeTypes} />
   </div>
 );
