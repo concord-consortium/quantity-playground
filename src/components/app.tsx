b0205086f962b71cebe28babb3b6667fd84bba82
Scott Cytacki <scytacki@concord.org>
2021-11-14 00:16:47 -0500

    remove extra components

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,11 @@
 import React from "react";
-import { Text } from "./text";
-import { useSampleText } from "../hooks/use-sample-text";
-import Icon from "../assets/concord.png";
-
-import "./app.scss";
 import { Diagram } from "./diagram";
 
+import "./app.scss";
+
 export const App = () => {
-  const sampleText = useSampleText();
   return (
     <div className="app">
-      <img src={Icon}/>
-      <Text text={sampleText} />
       <Diagram />
     </div>
   );
