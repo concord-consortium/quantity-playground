16c6ef333c9cf07488370eabd77d975942b92b86
Kirk Swenson <kswenson@concord.org>
2022-02-25 09:01:40 -0800

    Move codap connection handling to App component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,9 @@
+import { applySnapshot, getSnapshot, onSnapshot } from "mobx-state-tree";
 import React from "react";
 import { Diagram } from "../diagram/components/diagram";
 import { Operation } from "../diagram/models/dq-node";
 import { DQRoot } from "../diagram/models/dq-root";
+import codapInterface from "../lib/CodapInterface";
 
 import "./app.scss";
 
@@ -45,6 +47,50 @@ const loadInitialState = () => {
 };
 
 const dqRoot = DQRoot.create(loadInitialState());
+
+// For debugging
+(window as any).dqRoot = dqRoot;
+(window as any).getSnapshot = getSnapshot;
+
+// TODO: rewrite this as a reusable custom hook (useCodapConnection)
+const initializeCodapConnection = () => {
+  const codapConfig = {
+    customInteractiveStateHandler: true,
+    name: "Quantitative Playground",
+    version: "1.0"
+  };
+
+  codapInterface.on("get", "interactiveState", "",
+      () => {return {success: true, values: getSnapshot(dqRoot)};});
+
+  codapInterface.init(codapConfig).then(
+    (initialState) => {
+      if (initialState?.nodes) {
+        applySnapshot(dqRoot, initialState);
+        // when the model changes, notify CODAP that the plugin is 'dirty'
+        onSnapshot(dqRoot,() => {
+          codapInterface.sendRequest({
+            "action": "notify",
+            "resource": "interactiveFrame",
+            "values": {"dirty": true}
+          });
+        });
+      }
+      else {
+        codapInterface.sendRequest({
+          action: "update",
+          resource: "interactiveFrame",
+          values: {dimensions: {height: 600, width: 800}}
+        });
+      }
+    },
+    (msg: string) => {
+      console.warn("No CODAP: " + msg);
+    }
+  );
+};
+
+initializeCodapConnection();
 
 export const App = () => {
   return (
