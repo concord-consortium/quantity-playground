499b52f448dbca1bfb6845a9887f386c011df86f
Kirk Swenson <kswenson@concord.org>
2022-03-11 07:28:29 -0800

    Add comment

--- src/diagram/custom-mathjs.ts
+++ src/diagram/custom-mathjs.ts
@@ -1,3 +1,4 @@
+// Note: we can't use mathjs/number due to https://github.com/josdejong/mathjs/issues/2284
 import { create, simplifyDependencies } from "mathjs";
 
 // https://mathjs.org/docs/custom_bundling.html
