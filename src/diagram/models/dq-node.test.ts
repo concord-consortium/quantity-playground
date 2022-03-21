7183d02feb6f90bca6334fd820876ed5ccad9900
Scott Cytacki <scytacki@concord.org>
2022-03-21 16:47:03 -0400

    fix lint failure, and test

--- src/diagram/models/dq-node.test.ts
+++ src/diagram/models/dq-node.test.ts
@@ -1,3 +1,5 @@
+import { types } from "mobx-state-tree";
+import { AppStore } from "../../components/app-store";
 import { DQNode } from "./dq-node";
 import { Variable } from "./variable";
 
@@ -5,9 +7,23 @@ describe("DQNode", () => {
   it("can be instantiated with undefined value", () => {
     const variable = Variable.create({});
     const node = DQNode.create({ variable: variable.id, x: 0, y: 0 });
-    // but null is converted to undefined automatically via import
+
+    // references have to be within the same tree so we need some container 
+    const ContainerModel = types.model("ContainerModel", {
+      items: types.array(types.union(DQNode, Variable))
+    })
+    .actions(self => ({
+      add(item: any) {
+        self.items.push(item);
+      }
+    }));
+    const container = ContainerModel.create();
+    container.add(variable);
+    container.add(node);
+
     expect(node.variable.value).toBeUndefined();
   });
+
   // it("can't be instantiated with null value but can import null values", () => {
   //   // @ts-expect-error null value on create results in TypeScript error
   //   const node = DQNode.create({ value: null, x: 0, y: 0 });
