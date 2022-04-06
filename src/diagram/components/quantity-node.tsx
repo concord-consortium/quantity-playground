3b9bcd784325e29566ff90ea1e4eb7db1984ca46
eireland <eireland@concord.org>
2022-04-06 13:17:33 -0700

    Removes unused divs and styling

--- src/diagram/components/quantity-node.tsx
+++ src/diagram/components/quantity-node.tsx
@@ -66,64 +66,65 @@ const _QuantityNode: React.FC<IProps> = ({ data, isConnectable }) => {
       <div className="remove-node-button" onClick={handleRemoveNode} title={"Delete Node"}>
         <DeleteIcon />
       </div>
-      <div className="inputs-outputs">
-        <div className="inputs">
-            <div className="input">
-              <Handle
-                type="target"
-                position={Position.Left}
-                style={{top: "27%", left: "-12px", border: "1px solid white", borderRadius: " 12px", width: "24px", height: "24px", background: "#bcbcbc"}}
-                onConnect={(params) => console.log("handle onConnect", params)}
-                isConnectable={isConnectable}
-                id="a"
-              />
-              <Handle
-                type="target"
-                position={Position.Left}
-                style={{ top: "73%", left: "-12px", border: "1px solid white", borderRadius: " 12px", width: "24px", height: "24px", background: "#bcbcbc"}}
-                onConnect={(params) => console.log("handle onConnect", params)}
-                isConnectable={isConnectable}
-                id="b"
-
-              />
-            </div>
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{top: "27%", left: "-12px", border: "1px solid white", borderRadius: " 12px", width: "24px", height: "24px", background: "#bcbcbc"}}
+        onConnect={(params) => console.log("handle onConnect", params)}
+        isConnectable={isConnectable}
+        id="a"
+      />
+      <Handle
+        type="target"
+        position={Position.Left}
+        style={{ top: "73%", left: "-12px", border: "1px solid white", borderRadius: " 12px", width: "24px", height: "24px", background: "#bcbcbc"}}
+        onConnect={(params) => console.log("handle onConnect", params)}
+        isConnectable={isConnectable}
+        id="b"
+      />
+      <div className={"variable-info-container"}>
+        <div contentEditable="true" className={"variable-info name"} placeholder="name"
+             onMouseDown={e => e.stopPropagation()} onChange={onNameChange}>
+          {variable.name || ""}
         </div>
-        <div className={"variable-info-container"}>
-          <input className={"variable-info name"} placeholder="name" onChange={onNameChange} value={variable.name || ""} onMouseDown={e => e.stopPropagation()}/>
+        <div>
           <input className={"variable-info value"} type="number" placeholder="value" onChange={onValueChange}
             value={shownValue !== undefined ? shownValue.toString() : ""} onMouseDown={e => e.stopPropagation()}/>
-          <input className={"variable-info unit"} type="text" placeholder="unit" onChange={onUnitChange} value={shownUnit|| ""} onMouseDown={e => e.stopPropagation()}/>
+          <input className={"variable-info unit"} type="text" placeholder="unit" value={shownUnit|| ""}
+            onChange={onUnitChange} onMouseDown={e => e.stopPropagation()}/>
+        </div>
+        <div>
           <select className={"variable-info operation"} value={data.node.variable.operation || ""} onChange={onOperationChange}>
-            { // in an enumeration the keys are the names and the values are string ornumeric identifier
-            }
-            <option key="none" value="">none</option>
-            {Object.entries(Operation).map(([name, symbol]) =>
-              <option key={name} value={symbol}>{symbol}</option>
-            )}
+          { // in an enumeration the keys are the names and the values are string or numeric identifier
+          }
+          <option key="none" value="">none</option>
+          {Object.entries(Operation).map(([name, symbol]) =>
+            <option key={name} value={symbol}>{symbol}</option>
+          )}
           </select>
           { variable.computedValueError &&
-           <div className="error-message">
-               ⚠️ {variable.computedValueError}
-           </div>
-         }
-         { variable.computedUnitError &&
-           <div className="error-message">
-               ⚠️ {variable.computedUnitError}
-           </div>
-         }
-         { variable.computedUnitMessage &&
-           <div className="error-message">
-               ⓘ {variable.computedUnitMessage}
-           </div>
-         }
+            <div className="error-message">
+                ⚠️ {variable.computedValueError}
+            </div>
+          }
+          { variable.computedUnitError &&
+            <div className="error-message">
+                ⚠️ {variable.computedUnitError}
+            </div>
+          }
+          { variable.computedUnitMessage &&
+            <div className="error-message">
+                ⓘ {variable.computedUnitMessage}
+            </div>
+          }
         </div>
-       <Handle
-         type="source"
-         position={Position.Right}
-         isConnectable={isConnectable}
-         style={{border: "1px solid white", borderRadius: " 12px", width: "24px", height: "24px", right: "-12px", background: "#bcbcbc"}}
-       />
       </div>
+      <Handle
+        type="source"
+        position={Position.Right}
+        isConnectable={isConnectable}
+        style={{border: "1px solid white", borderRadius: " 12px", width: "24px", height: "24px", right: "-12px", background: "#bcbcbc"}}
+      />
     </div>
   );
 };
