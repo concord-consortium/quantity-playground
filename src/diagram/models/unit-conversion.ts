0938b543892d58189a867752771efccc5c4234b9
Scott Cytacki <scytacki@concord.org>
2022-04-09 10:20:51 -0400

    remove unit conversion it isn't used anymore

--- src/diagram/models/unit-conversion.ts
+++ src/diagram/models/unit-conversion.ts
@@ -2,8 +2,6 @@
 import { SymbolNode } from "mathjs";
 import { unit, parse, createUnit, Unit } from "../custom-mathjs";
 
-type ConversionFunction = (input: number) => number;
-
 export const getMathUnit = (value: number, unitString: string) => {
   try {
     // Look for unknown units in the unit string
@@ -20,27 +18,4 @@ export const getMathUnit = (value: number, unitString: string) => {
   } catch (e: any) {
     // Do nothing just return undefined
   }
-};
-
-export const getUnitConversion = (inputUnit:string, outputUnit:string): ConversionFunction | null => {
-  
-  if (inputUnit === outputUnit) {
-      return value => value;
-  }
-
-  // First check if we can actual do this conversion by checking the bases of
-  // the two units.  I'm not sure if this will handle compound units.
-  
-  const inputMathUnit = getMathUnit(1, inputUnit);
-  const outputMathUnit = getMathUnit(1, outputUnit);
-
-  if (!inputMathUnit || !outputMathUnit) {
-      return null;
-  }
-
-  if (!inputMathUnit.equalBase(outputMathUnit)) {
-    return null;
-  }
-
-  return value => unit(value, inputUnit).toNumber(outputUnit);
 };
