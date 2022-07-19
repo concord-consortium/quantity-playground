188f88f2e0abacacaa4585921b45ac5a585b85f6
Jonathan Sandoe <jsandoe@concord.org>
2022-07-19 14:55:00 -0700

    Color picker to select background color for quantity node

---
+++ src/diagram/components/color-editor.tsx
@@ -0,0 +1,42 @@
+import React, { /*useEffect, useRef,*/ useState } from "react";
+//import { useClickAway } from "../../hooks/use-click-away";
+import { VariableType } from "../models/variable";
+
+import "./color-editor.scss";
+import {CirclePicker} from "react-color";
+
+interface IProps {
+  variable: VariableType
+  onShowColorEditor: (show: boolean) => void;
+}
+
+export const ColorEditor: React.FC<IProps> = ({variable, onShowColorEditor}) => {
+
+  const [color, setColor] = useState(variable.color);
+
+  const handleColorChange = (evt:any) => {
+    variable.setColor(evt.hex);
+    setColor(evt.hex);
+    onShowColorEditor(false);
+  };
+
+  return (
+    <div className="color-editor-dialog">
+      <div className="variable-info-row ">
+        <CirclePicker color={color} onChange={handleColorChange} width={"110px"} colors={
+          [
+            "#FF6900",
+            "#e98b42",
+            "#7BDCB5",
+            "#00D084",
+            "#8ED1FC",
+            "#0693E3",
+            "#ABB8C3",
+            "#EB144C",
+            "#F78DA7",
+            "#9900EF"
+          ]} circleSize={14} circleSpacing={7}/>
+      </div>
+    </div>
+  );
+};
--- src/diagram/components/quantity-node.tsx
+++ src/diagram/components/quantity-node.tsx
@@ -5,6 +5,7 @@ import { Handle, Position } from "react-flow-renderer/nocss";
 import { DQNodeType } from "../models/dq-node";
 import { DQRootType } from "../models/dq-root";
 import { ExpressionEditor } from "./expression-editor";
+import { ColorEditor } from "./color-editor";
 
 import "./quantity-node.scss";
 
@@ -22,6 +23,22 @@ const EditIcon = () =>
   </svg>
 ;
 
+const ColorPickerIcon = () =>
+    <svg width="36px" height="36px" viewBox="0 0 36 36" version="1.1"
+         preserveAspectRatio="xMidYMid meet"
+         xmlns="http://www.w3.org/2000/svg">
+      <title>color-picker-solid</title>
+      <path
+          d="M33.73,2.11a4.09,4.09,0,0,0-5.76.1L22.81,7.38a3.13,3.13,0,0,1-4.3.11L17.09,8.91,27,18.79l1.42-1.42A3.18,3.18,0,0,1,28.46,13l5.17-5.17A4.08,4.08,0,0,0,33.73,2.11Z"
+          className="clr-i-solid clr-i-solid-path-1">
+      </path>
+      <path
+          d="M22.18,16.79,7.46,31.51a2,2,0,1,1-2.82-2.83L19.35,14l-1.41-1.41L3.22,27.27a4,4,0,0,0-.68,4.8L1.06,33.55a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l1.44-1.44a3.93,3.93,0,0,0,2.09.6,4.06,4.06,0,0,0,2.88-1.2L23.6,18.21Z"
+          className="clr-i-solid clr-i-solid-path-2">
+      </path>
+    </svg>
+;
+
 interface IProps {
   data: {node: DQNodeType, dqRoot: DQRootType};
   isConnectable: boolean;
@@ -29,6 +46,7 @@ interface IProps {
 
 const _QuantityNode: React.FC<IProps> = ({ data, isConnectable }) => {
   const [showExpressionEditor, setShowExpressionEditor] = useState(false);
+  const [showColorEditor, setShowColorEditor] = useState(false);
 
   // When the node is removed from MST, this component gets
   // re-rendered for some reason, so we check here to make sure we
@@ -45,6 +63,10 @@ const _QuantityNode: React.FC<IProps> = ({ data, isConnectable }) => {
 
   const handleEditExpression = (show: boolean) => {
     setShowExpressionEditor(show);
+  };
+
+  const handleEditColor = (show: boolean) => {
+    setShowColorEditor(show);
   };
 
   const onValueChange = (evt: any) => {
@@ -102,7 +124,7 @@ const _QuantityNode: React.FC<IProps> = ({ data, isConnectable }) => {
   const sourceHandleStyle = {border: "1px solid white", borderRadius: "6px", width: "12px", height: "12px", background: "#bcbcbc"};
 
   return (
-    <div className={`node ${hasExpression ? "expression-shown" : ""}`} data-testid="quantity-node">
+    <div className={`node ${hasExpression ? "expression-shown" : ""}`} style={{background:variable.color}} data-testid="quantity-node">
       <div className="remove-node-button" onClick={handleRemoveNode} title={"Delete Node"} data-testid={"delete-node-button"}>
         <DeleteIcon/>
       </div>
@@ -156,6 +178,12 @@ const _QuantityNode: React.FC<IProps> = ({ data, isConnectable }) => {
         title="drag to connect"
       />
       {showExpressionEditor && <ExpressionEditor variable={variable} onShowExpressionEditor={handleEditExpression}/>}
+      <div className="variable-info-floater">
+        <div className="edit-color-button" onClick={()=>handleEditColor(true)} title={"Edit Color"}  data-testid="color-edit-button">
+          <ColorPickerIcon/>
+        </div>
+        {showColorEditor && <ColorEditor variable={variable} onShowColorEditor={handleEditColor}/>}
+      </div>
     </div>
   );
 };
