0718623faa92eec55810da3c08780d8c2305cf3a
Teale Fristoe <fristoe@gmail.com>
2022-10-20 13:39:56 -0700

    Export EditVariableDialog and updateVariable

--- src/diagram/index.ts
+++ src/diagram/index.ts
@@ -1,8 +1,9 @@
 import { Diagram } from "./components/diagram";
+import { EditVariableDialogContent, updateVariable } from "./components/dialog/edit-variable-dialog";
 import { DQNode } from "./models/dq-node";
 import { DQRoot } from "./models/dq-root";
 import { Variable, VariableType } from "./models/variable";
 
 import "./components/diagram.scss";
 
-export { Diagram, DQNode, DQRoot, Variable, VariableType};
+export { Diagram, EditVariableDialogContent, updateVariable, DQNode, DQRoot, Variable, VariableType};
