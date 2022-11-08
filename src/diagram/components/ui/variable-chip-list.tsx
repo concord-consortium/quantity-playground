13018a188649f713b9c188953aa2e403f07d46e5
Teale Fristoe <fristoe@gmail.com>
2022-11-08 11:23:35 -0800

    Allow selecting multiple chips from a list

--- src/diagram/components/ui/variable-chip-list.tsx
+++ src/diagram/components/ui/variable-chip-list.tsx
@@ -8,22 +8,25 @@ import "./variable-chip-list.scss";
 interface IVariableChipList {
   nameOnly?: boolean;
   onClick?: (variable: VariableType) => void;
-  selectedVariable?: VariableType;
+  selectedVariables?: VariableType | VariableType[];
   variables: VariableType[];
 }
-export const VariableChipList = ({ nameOnly, onClick, selectedVariable, variables }: IVariableChipList) => {
+export const VariableChipList = ({ nameOnly, onClick, selectedVariables, variables }: IVariableChipList) => {
   return (
     <div className="variable-chip-list">
       {
-        variables.map(variable => (
-          <VariableChip
+        variables.map(variable => {
+          const selected = Array.isArray(selectedVariables)
+            ? selectedVariables.includes(variable)
+            : selectedVariables === variable;
+          return <VariableChip
             key={variable.id}
             nameOnly={nameOnly}
             onClick={onClick}
-            selected={selectedVariable === variable}
+            selected={selected}
             variable={variable}
-          />
-        ))
+          />;
+        })
       }
     </div>
   );
