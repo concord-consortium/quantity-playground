39b00fe70ebfa4cfecdabcb571b8fbfcb89e460b
Teale Fristoe <fristoe@gmail.com>
2022-11-27 16:17:46 -0800

    Allow client to hide minimap

--- src/diagram/components/diagram.tsx
+++ src/diagram/components/diagram.tsx
@@ -31,6 +31,7 @@ interface IProps {
   externalRfInstance?: any;
   externalSetRfInstance?: (val: any) => void;
   hideControls?: boolean;
+  hideMinimap?: boolean;
   hideNewVariableButton?: boolean;
   showDeleteCardButton?: boolean;
   showEditVariableDialog?: () => void;
@@ -39,7 +40,7 @@ interface IProps {
   getDiagramExport?: () => unknown;
 }
 export const _Diagram = ({ dqRoot, externalReactFlowWrapper, externalRfInstance, externalSetRfInstance, getDiagramExport,
-  hideControls, hideNewVariableButton, showDeleteCardButton, showEditVariableDialog, showUnusedVariableDialog }: IProps) => 
+  hideControls, hideMinimap, hideNewVariableButton, showDeleteCardButton, showEditVariableDialog, showUnusedVariableDialog }: IProps) => 
 {
   const internalReactFlowWrapper = useRef<any>(null);
   const [internalRfInstance, internalSetRfInstance] = useState<any>();
@@ -182,7 +183,7 @@ export const _Diagram = ({ dqRoot, externalReactFlowWrapper, externalRfInstance,
           onNodeDrag={onNodeDrag}
           onNodeDragStop={onNodeDragStop}
           onMoveEnd={handleChangeFlowTransform}>
-          <MiniMap/>
+          {!hideMinimap && <MiniMap/>}
           {!hideControls && <Controls />}
           <ToolBar {...{deleteCard, dqRoot, getDiagramExport, hideNewVariableButton, showEditVariableDialog, showUnusedVariableDialog}}/>
         </ReactFlow>
