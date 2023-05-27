e80719dfe30a3328f6606b497a64453b0f8ed3be
Teale Fristoe <fristoe@gmail.com>
2023-05-26 20:54:57 -0700

    Convert some of quantity-node test to cypress.

---
+++ cypress/integration/quantity-node.test.ts
@@ -0,0 +1,65 @@
+import {
+  circlePicker, colorPaletteButton, enterExpression, enterName, enterNotes, enterUnit,
+  enterValue, expressionField, legalNotes, longNotes, nameField, node, notesButton, notesField, unitField, valueField
+} from "../support/diagram-helpers";
+import { processName } from "../../src/diagram/utils/validate";
+
+context("Test Diagram interaction", () => {
+  beforeEach(() => {
+    cy.visit("");
+  });
+
+  const nodeIndex = 1;
+  const nodeToEdit = () => node(nodeIndex);
+
+  describe("Variable Cards", () => {
+    it("can enter basic input", async () => {
+      // Enter name, value, and unit
+      const variableName = "my variable name";
+      enterName(nodeIndex, variableName);
+      nameField(nodeIndex).should("contain.value", processName(variableName));
+      const variableValue = "45";
+      enterValue(nodeIndex, variableValue);
+      valueField(nodeIndex).should("contain.value", variableValue);
+      const variableUnit = "miles";
+      enterUnit(nodeIndex, variableUnit);
+      unitField(nodeIndex).should("contain.value", variableUnit);
+
+      // Illegal values display as invalid
+      enterValue(nodeIndex, "letter");
+      valueField(nodeIndex).should("have.class", "invalid");
+
+      // Enter description
+      const variableDescription = "a\ndescription";
+      notesField(nodeIndex).should("not.exist");
+      notesButton(nodeIndex).click();
+      enterNotes(nodeIndex, variableDescription);
+      notesField(nodeIndex).should("contain.value", variableDescription);
+
+      // Notes are limited to kMaxNotesCharacters
+      notesField(nodeIndex).clear();
+      enterNotes(nodeIndex, longNotes);
+      notesField(nodeIndex).should("contain.value", legalNotes);
+    });
+  });
+
+  describe("Variable Cards", () => {
+    it("can change color", async () => {
+      nodeToEdit().should("have.class", "light-gray");
+      circlePicker(nodeIndex).should("not.exist");
+      colorPaletteButton(nodeIndex).click();
+      circlePicker(nodeIndex).should("exist");
+      circlePicker(nodeIndex).find("span div span div").eq(5).click();
+      nodeToEdit().should("have.class", "red");
+    });
+  });
+
+  describe("Variable Cards", () => {
+    it("can enter expressions", async () => {
+      expressionField(nodeIndex).should("not.exist");
+      const variableExpression = "9+9";
+      enterExpression(variableExpression);
+      expressionField().should("have.value", variableExpression);
+    });
+  });
+});
