e168a6d979afb9cb55f90ba3e925fd16d8ef8aff
Teale Fristoe <fristoe@gmail.com>
2023-05-31 06:33:26 -0700

    Update comment about memoizing QuantityNode.

--- src/diagram/components/quantity-node.tsx
+++ src/diagram/components/quantity-node.tsx
@@ -230,19 +230,8 @@ const _QuantityNode: React.FC<IProps> = ({ data, isConnectable }) => {
   );
 };
 
-// In the custom node example memo is used here, but when I
-// used it then the component was updating when it was marked
-// as an observer and its model changed. So I'd guess memo
-// might get in the way of observer.
-// export const QuantityNode = memo(observer(_QuantityNode));
-
-// Also with testing the observer isn't needed for simple changes
-// like deleting edges or connecting edges.
-// My guess is that Flow re-renders on all changes like this
-// as long as the change triggers this re-render we are fine.
-//
-// But if the model gets changed without a flow re-render
-// then, it doesn't update without the observer
+// Reactflow recommends memoizing custom node types. See:
+// https://reactflow.dev/docs/guides/migrate-to-v10/#2-memoize-your-custom-nodetypes-and-edgetypes
 export const QuantityNode = memo(observer(_QuantityNode));
 // Because it is observed we have to set the display name
 QuantityNode.displayName = "QuantityNode";
