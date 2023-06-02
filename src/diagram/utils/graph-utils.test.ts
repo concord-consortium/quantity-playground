d312cfcb73d2636fabfe75bc4be56a526be868ee
Teale Fristoe <fristoe@gmail.com>
2023-06-02 14:23:02 -0700

    Expand graph-utils test slightly.

--- src/diagram/utils/graph-utils.test.ts
+++ src/diagram/utils/graph-utils.test.ts
@@ -6,10 +6,12 @@ describe("Discovers illegal inputs", () => {
   const a = Variable.create({ id: "a", name: "a" });
   const b = Variable.create({ id: "b", name: "b" });
   const c = Variable.create({ id: "c", name: "c" });
+  const d = Variable.create({ id: "d", name: "d" });
   const container = GenericContainer.create();
   container.add(a);
   container.add(b);
   container.add(c);
+  container.add(d);
 
   it("cannot connect to oneself", () => {
     expect(canAddInput(a, a)).toEqual(false);
@@ -25,11 +27,18 @@ describe("Discovers illegal inputs", () => {
   });
 
   it("cannot connect to ancestors but can connect to descendents", () => {
-    expect(canAddInput(c, b)).toEqual(true);
-    expect(canAddInput(c, a)).toEqual(true);
+    expect(canAddInput(d, c)).toEqual(true);
+    expect(canAddInput(d, b)).toEqual(true);
+    expect(canAddInput(d, a)).toEqual(true);
+    d.addInput(c);
     c.addInput(b);
+    expect(canAddInput(d, c)).toEqual(false);
+    expect(canAddInput(d, b)).toEqual(false);
+    expect(canAddInput(d, a)).toEqual(false);
     expect(canAddInput(c, b)).toEqual(false);
     expect(canAddInput(c, a)).toEqual(false);
+    expect(canAddInput(b, d)).toEqual(true);
     expect(canAddInput(a, c)).toEqual(true);
+    expect(canAddInput(a, d)).toEqual(true);
   });
 });
