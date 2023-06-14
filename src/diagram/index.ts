b422d5a08e22cc112a391d4b038aa7281ff111b5
Teale Fristoe <fristoe@gmail.com>
2023-06-14 07:53:53 -0700

    Export variable snapshot type.

--- src/diagram/index.ts
+++ src/diagram/index.ts
@@ -4,7 +4,7 @@ import { VariableChip } from "./components/ui/variable-chip";
 import { VariableChipList } from "./components/ui/variable-chip-list";
 import { DQNode } from "./models/dq-node";
 import { DQRoot } from "./models/dq-root";
-import { Variable, VariableType } from "./models/variable";
+import { Variable, VariableSnapshot, VariableType } from "./models/variable";
 import { DiagramHelper } from "./utils/diagram-helper";
 import { useSelectMultipleVariables } from "../hooks/use-select-multiple-variables";
 
@@ -12,5 +12,5 @@ import "./components/diagram.scss";
 
 export {
   Diagram, DiagramHelper, EditVariableDialogContent, updateVariable, DQNode, DQRoot, useSelectMultipleVariables,
-  Variable, VariableChip, VariableChipList, VariableType
+  Variable, VariableChip, VariableChipList, VariableSnapshot, VariableType
 };
