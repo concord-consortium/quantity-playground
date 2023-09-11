c24b814145326ac6534c9579f14d3da946df6cd0
Teale Fristoe <fristoe@gmail.com>
2023-09-11 11:38:45 -0700

    Add variable slider.

--- src/diagram/index.ts
+++ src/diagram/index.ts
@@ -2,6 +2,7 @@ import { Diagram } from "./components/diagram";
 import { EditVariableDialogContent, updateVariable } from "./components/dialog/edit-variable-dialog";
 import { VariableChip } from "./components/ui/variable-chip";
 import { VariableChipList } from "./components/ui/variable-chip-list";
+import { VariableSlider } from "./components/ui/variable-slider";
 import { DQNode } from "./models/dq-node";
 import { DQRoot } from "./models/dq-root";
 import { Variable, VariableSnapshot, VariableType } from "./models/variable";
@@ -12,5 +13,5 @@ import "./components/diagram.scss";
 
 export {
   Diagram, DiagramHelper, EditVariableDialogContent, updateVariable, DQNode, DQRoot, useSelectMultipleVariables,
-  Variable, VariableChip, VariableChipList, VariableSnapshot, VariableType
+  Variable, VariableChip, VariableChipList, VariableSlider, VariableSnapshot, VariableType
 };
