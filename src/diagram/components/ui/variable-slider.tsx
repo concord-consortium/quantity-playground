a022f6aad737839262de9ef01d1fe7bd0a1c29f2
Teale Fristoe <fristoe@gmail.com>
2023-09-11 12:08:07 -0700

    Use temporary values in slider.

--- src/diagram/components/ui/variable-slider.tsx
+++ src/diagram/components/ui/variable-slider.tsx
@@ -13,12 +13,16 @@ interface IVariableSliderProps {
   variable?: VariableType;
 }
 export function VariableSlider({ className, max, min, step, variable}: IVariableSliderProps) {
+  const handleChange = (value: number | number[]) =>
+    variable?.setTemporaryValue(Array.isArray(value) ? value[0] : value);
+  const handleAfterChange = (value: number | number[]) => variable?.commitTemporaryValue();
   return (
     <Slider
       className={className}
       max={max}
       min={min}
-      onChange={(value: number | number[]) => variable?.setValue(Array.isArray(value) ? value[0] : value)}
+      onAfterChange={handleAfterChange}
+      onChange={handleChange}
       step={step}
       value={variable?.value ?? min}
     />
