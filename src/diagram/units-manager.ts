1f28b9de57d6659c48bdf7bd21618bb7bcf107e0
Scott Cytacki <scytacki@concord.org>
2024-10-07 15:56:39 -0400

    initializeMath can't be an action
    - add a cypress test for the case that exposes the problem with it being an action
    - add comments explaining the complexity of getMathUnit

--- src/diagram/units-manager.ts
+++ src/diagram/units-manager.ts
@@ -24,7 +24,8 @@ export class UnitsManager {
     this.units.push({ unit, options });   
   }
 
-  @action
+  // This is not an action because we want changes in the units to be observed
+  // and trigger initializeMath to be run again
   initializeMath(m: math.MathJsStatic) {
     // The types don't give access to the Unit class object, but it is there.
     const mathUnit = (m as any).Unit;
@@ -53,12 +54,33 @@ export class UnitsManager {
 
   // NOTE: this is both an action and a view. It is called by MST views to get the math.Unit
   // object. At the same time is possibly adding units to the units array of `this`. The addition
-  // of these new units should trigger updates in other views that are looking for these units.
-  // This function does not actually "read" any observable state. So it is OK that it is treated
+  // of these new units triggers updates in other views that are observing the units via the 
+  // initializeMath method above.
+  // The `getMathUnit` function does not actually "read" any observable state. So it is OK that it is treated
   // as an action by MobX.
-  // However there might be some case of a circular update where view calling this is observing
-  // units array itself.
-  // TODO: find a way to separate these two parts.
+  //
+  // The interaction of this within the full system is complex. Below is a list of what happens current.
+  // However if the code is changed slightly, this might be out of date. When a equation is entered that uses
+  // an input variable that has units:
+  // - `computedValueIncludingMessageAndError` called on equation variable 
+  // - this calls `mathValue` on the input variable
+  // - `mathValue` calls `self.math` which causes `unitsManager.initializeMath` to be called on the 
+  //   mathLib of the input variable.
+  // - then `unitsManager.getMathUnit` is called with this mathLib from the input variable
+  // - this adds the units of the input variable to the unitsManager.units array.
+  // - a temporary `mathValue` is returned for the input variable
+  // - `computedValueIncludingMessageAndError` returns a value for the equation variable.
+  // - because the units array was updated, and `self.math` is used by `mathValue`, both this triggers
+  //   any observers of the mathValue to be recomputed.
+  // - this includes the `computedValueIncludingMessageAndError` on the equation variable.
+  // - because of this, `unitsManager.getMathUnit` is called again, but this time the units exist
+  //   so an endless loop does not happen.
+  // 
+  // TODO: Find a way to separate the action and view so the complex interaction above is simplified.
+  // With the current setup I think the only way to do that is to add the units to the UnitsManager
+  // as they are defined in the variables. And if we do that we probably should also clean up units
+  // that are no longer used by variables. With that change, this `getMathUnit` could just be a 
+  // "view".
   @action
   getMathUnit(value: number, unitString: string, mathLib: IMathLib): math.Unit | undefined {
     try {
@@ -75,6 +97,11 @@ export class UnitsManager {
               if (mathLib.Unit.isValuelessUnit(singular) || mathLib.Unit.isValuelessUnit(plural)) {
                 // If the singular or plural is already a unit, just add the base
                 this.addUnit(symbol.name);
+                // NOTE: this `createUnit` call and the 2 other below are only adding the unit to the passed 
+                // in mathLib. Each variable has its own mathLib, so the other variable's mathLibs are not be
+                // updated here.
+                // The system relies on MobX observing for those other variables to update their mathLibs
+                // when a new unit is added by the `this.addUnit` above.
                 mathLib.createUnit(symbol.name);
               } else {
                 // Otherwise, add them both as synonyms
